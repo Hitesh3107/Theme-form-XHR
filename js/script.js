9835e5fbc75570f24c7a684d232834b337892818
789349 <vinay.dubey0900@gmail.com>
2022-05-25 20:47:36 +0530

    vinay done changes

--- js/script.js
+++ js/script.js
@@ -9,36 +9,118 @@
 */
 
 const themes = {
-    "bright": {
-        "backgroundColor": "lightyellow",
-        "color": "darkgreen"
-    },
-    "cool": {
-        "backgroundColor": "lightgray",
-        "color": "indigo"
-    },
-    "fiery": {
-        "backgroundColor": "chocolate",
-        "color": "lightgoldenyellow"
-    }
-}
+  bright: {
+    backgroundColor: "lightyellow",
+    color: "darkgreen",
+  },
+  cool: {
+    backgroundColor: "lightgray",
+    color: "indigo",
+  },
+  fiery: {
+    backgroundColor: "chocolate",
+    color: "lightgoldenyellow",
+  },
+};
 
 function changeTheme(event) {
-    console.log(event);
-switch(event.target.id){
+  switch (event.target.id) {
     case "bright":
-        document.body.style.backgroundColor=themes.bright.backgroundColor;
-        document.body.style.color=themes.bright.color;
-        break;
+      document.body.style.backgroundColor = themes.bright.backgroundColor;
+      document.body.style.color = themes.bright.color;
+      break;
     case "cool":
-        document.body.style.backgroundColor=themes.cool.backgroundColor;
-        document.body.style.color=themes.cool.color;
-        break;
+      document.body.style.backgroundColor = themes.cool.backgroundColor;
+      document.body.style.color = themes.cool.color;
+      break;
     case "fiery":
-        document.body.style.backgroundColor=themes.fiery.backgroundColor;
-        document.body.style.color=themes.fiery.color;
-        break;
-}  
+      document.body.style.backgroundColor = themes.fiery.backgroundColor;
+      document.body.style.color = themes.fiery.color;
+      break;
+  }
 }
 
-module.exports = changeTheme
+// Getting dynamic data in card
+
+let dynamicData = document.getElementById("dynamicData");
+var data = [];
+(function getData() {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", "../data/data.json");
+  xhr.responseType = "json";
+  xhr.onload = () => {
+    data = xhr.response;
+    data.forEach((ele) => {
+      let card = `<div class="card col-md-3 my-card">
+            <div class="card-body">
+              <h5 class="card-title ">${ele.title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${ele.price}</h6>
+              <p class="card-text">
+                ${ele.description.substring(0, 50)}
+              </p>
+            </div>
+          </div>`;
+      dynamicData.innerHTML += card;
+    });
+  };
+  xhr.send();
+})();
+
+// validate form
+function validateForm(e) {
+  e.preventDefault();
+  let firstname = document.getElementById("firstname").value;
+  let lastname = document.getElementById("lastname").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+
+  let city = document.getElementById("city").value;
+  let state = document.getElementById("state").value;
+  let zip = document.getElementById("zip").value;
+
+  let gender = getGender();
+  let maritalStatus = getMaritalStatus();
+
+  let entry = {
+    firstname: firstname,
+    lastname: lastname,
+    email: email,
+    phone: phone,
+    city: city,
+    state: state,
+    gender: gender,
+    maritalStatus: maritalStatus,
+    zip: zip,
+  };
+
+  console.log(entry);
+}
+
+function getMaritalStatus() {
+  //   Married
+  let married = document.getElementById("married").checked;
+  let single = document.getElementById("single").checked;
+
+  if (married) {
+    return "married";
+  }
+  if (single) {
+    return "single";
+  }
+}
+function getGender() {
+  //   Gender
+  let male = document.getElementById("male").checked;
+  let female = document.getElementById("female").checked;
+  let other = document.getElementById("other").checked;
+
+  if (male) {
+    return "male";
+  }
+  if (female) {
+    return "female";
+  }
+  if (other) {
+    return "other";
+  }
+}
